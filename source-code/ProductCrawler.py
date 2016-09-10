f9e26d99bc0a8745ddfca69f181c2b3c73fc1ea5
vhnam <vhnam2504@gmail.com>
2016-09-11 03:49:08 +0700

    Fix error Accepting Cookies and crawl related product links.

---
+++ source-code/ProductCrawler.py
@@ -0,0 +1,57 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+from ReviewsCrawler import ReviewsCrawler
+
+class ProductCrawler:
+    url = ''
+    headers = {}
+
+    def __init__(self, keyword):
+        keyword = keyword.lower()
+        keyword = keyword.replace(' ', '+')
+        self.url = 'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + keyword
+        self.setHeader()
+
+    def setHeader(self):
+        self.headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36',
+            'Cookie': 'x-wl-uid=1ufpx359OBHL3vTgz/MbR7JgvkGJQRhz3LckdkNugbfHLCGPk7vetOZISd2oWd2WSJfsVdg2IorQ=; aws-session-id=160-6219495-6698944; aws-session-id-time=2102609170l; aws-ubid-main=158-6490660-4063723; s_pers=%20s_vnum%3D1474481169449%2526vn%253D1%7C1474481169449%3B%20s_invisit%3Dtrue%7C1471890969449%3B; session-token="GFYnOfaa8viObgPPKTFbjsjtj/hdS0xHe8XNxHlZ6bDpw/7cg59WOVThx4zcoPrv5qYG69uqL0Excw9ips2zxkJ/BZsdEYRMnlblx4vGeno59jEosFKNgWwelK2xxDUCyus3M8Sk+kb5jnJVHFfeBrN6g1Flud/Dn0YCpygq7J3MZSdA1GGRGS9DsL45rMKGSpXeKVdF0ObGtEfhUMXYlg=="; session-id-time=2082787201l; session-id=164-0077019-9402407; ubid-main=191-7802258-0837316; csm-hit=J8ZWM3K7DF32THMS1ZQ4+s-WETX416M14AVQ4564431|1473531641764'
+        }
+
+    def parseHMTL(self, url):
+        html = requests.get(url, headers = self.headers)
+        return html.content
+
+    def crawl(self):
+        nextLink = self.url
+        file = open('./amazon-products.html', 'w+')
+        page = 1
+        isCrawling = True
+
+        while isCrawling:
+            print 'Parsing page ' + str(page)
+            page = page + 1
+            html = self.parseHMTL(nextLink)
+            soup = BeautifulSoup(html)
+            nextLink = soup.find('a', {'id': 'pagnNextLink'})
+            productLinks = soup.findAll('li', {'class': 's-result-item celwidget'})
+
+            for link in productLinks:
+                productID = link['data-asin']
+                crawler = ReviewsCrawler(productID)
+                crawler.getReviews()
+
+            if nextLink is None:
+                isCrawling = False
+            else:
+                nextLink = 'https://www.amazon.com' + nextLink['href']
+                print >> file, soup.findAll('a', {'class': 'a-link-normal s-access-detail-page  a-text-normal'})
+
+        print 'Done!'
+
+crawler = ProductCrawler('Vincent Bach Trumpet')
+crawler.crawl()
--- source-code/ReviewsCrawler.py
+++ source-code/ReviewsCrawler.py
@@ -1,4 +1,3 @@
-import urllib
 import csv
 import requests
 from BeautifulSoup import BeautifulSoup
@@ -6,16 +5,27 @@ from BeautifulSoup import BeautifulSoup
 class ReviewsCrawler:
     url = ''
     links = 0
-    proxy = '169.50.87.252:80'
     outputFile = './amazon-reviews.csv'
-    session = requests.Session()
+    headers = {}
 
     def __init__(self, productID):
+        print '--- Crawling Product: ' + productID +' ---'
         print 'Start Contructor'
+        self.setHeader()
         self.url = 'https://www.amazon.com/product-reviews/' + productID + '/?showViewpoints=0&sortBy=byRankDescending'
         html = self.parseHTML(self.url)
         self.links = self.getNumberOfLinks(html)
         print 'Complete Contructor'
+
+    def setHeader(self):
+        self.headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36',
+            'Cookie': 'x-wl-uid=1ufpx359OBHL3vTgz/MbR7JgvkGJQRhz3LckdkNugbfHLCGPk7vetOZISd2oWd2WSJfsVdg2IorQ=; aws-session-id=160-6219495-6698944; aws-session-id-time=2102609170l; aws-ubid-main=158-6490660-4063723; s_pers=%20s_vnum%3D1474481169449%2526vn%253D1%7C1474481169449%3B%20s_invisit%3Dtrue%7C1471890969449%3B; session-token="GFYnOfaa8viObgPPKTFbjsjtj/hdS0xHe8XNxHlZ6bDpw/7cg59WOVThx4zcoPrv5qYG69uqL0Excw9ips2zxkJ/BZsdEYRMnlblx4vGeno59jEosFKNgWwelK2xxDUCyus3M8Sk+kb5jnJVHFfeBrN6g1Flud/Dn0YCpygq7J3MZSdA1GGRGS9DsL45rMKGSpXeKVdF0ObGtEfhUMXYlg=="; session-id-time=2082787201l; session-id=164-0077019-9402407; ubid-main=191-7802258-0837316; csm-hit=J8ZWM3K7DF32THMS1ZQ4+s-WETX416M14AVQ4564431|1473531641764'
+        }
 
     def getNumberOfLinks(self, html):
         soup = BeautifulSoup(html)
@@ -23,7 +33,7 @@ class ReviewsCrawler:
         return len(pagelinks)
 
     def parseHTML(self, url):
-        html = requests.get(url, proxies={"http": self.proxy})
+        html = requests.get(self.url, headers = self.headers)
         return html.content
 
     def parseReviews(self, html):
@@ -36,9 +46,17 @@ class ReviewsCrawler:
         date = review.find('span', {'class': 'a-size-base a-color-secondary review-date'})
         content = review.find('span', {'class': 'a-size-base review-text'})
 
+        if author is None:
+            author = review.find('span', {'class': 'a-size-base a-color-secondary review-byline'})
+
         with open(self.outputFile, 'ab') as fp:
             file = csv.writer(fp, delimiter = ',',quoting=csv.QUOTE_MINIMAL)
-            data = [str(author.text.encode('utf8')), str(rating.text[:3]), str(date.text[3:]), str(content.text.encode('utf8'))]
+            data = [
+                str(author.text.encode('utf8')),
+                str(rating.text[:3]),
+                str(date.text[3:]),
+                str(content.text.encode('utf8'))
+            ]
             file.writerow(data)
 
     def getReviews(self):
@@ -51,7 +69,5 @@ class ReviewsCrawler:
             print 'Total: ' + str(len(reviews)) + ' reviews'
             for review in reviews:
                 self.parseReview(review)
-            print 'Parsing completed!'
 
-crawler = ReviewsCrawler('B0002E1P08')
-crawler.getReviews()
+        print '--- Crawling completed! ---'
